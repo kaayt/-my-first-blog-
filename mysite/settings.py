3b8140be1f5b390ac4aded4517191500275948f6
Alex <a.kovalenko@gmail.com>
2017-01-11 15:54:42 +0200

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i)=hc#(9s27wv(e51i*=!)-j049e)$&$xk5&%qu26$p&%r&5+m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kaayt.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
